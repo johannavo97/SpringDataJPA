52d5089c792578605462ea967d7e31030be58b73
Johanna Vo <thanhtruc2009@gmail.com>
2022-06-13 11:05:19 -0700

    updated app

--- src/main/java/com/perscholas/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/com/perscholas/springdatajpa/SpringDataJpaApplication.java
@@ -1,13 +1,32 @@
 package com.perscholas.springdatajpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringDataJpaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDataJpaApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner run(EmployeeRepository repository){
+        return  (args -> {
+            insertJavaAdvocates(repository);
+            System.out.println(repository.findAll());
+        });
+
+    }
+
+    private void insertJavaAdvocates(EmployeeRepository repository){
+        repository.save(new Employee("Dalia", "Ado Sheasha"));
+        repository.save(new Employee("Trisha", "Gee"));
+        repository.save(new Employee("Helen", "Scott"));
+        repository.save(new Employee("Mala", "Gupta"));
+
     }
 
 }
