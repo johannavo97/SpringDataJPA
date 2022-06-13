c1f09df21101eda613839818dab462d8396e32d5
Johanna Vo <thanhtruc2009@gmail.com>
2022-06-13 13:42:22 -0700

    Finished

--- src/main/java/com/perscholas/springdatajpa/EmployeeRepository.java
+++ src/main/java/com/perscholas/springdatajpa/EmployeeRepository.java
@@ -2,5 +2,8 @@ package com.perscholas.springdatajpa;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+    List<Employee> findEmployeeByLastNameContaining(String str);
 }
